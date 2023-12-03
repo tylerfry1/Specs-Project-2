968b65ce8a972b479b62a87b14d0a1e8d05342f6
tylerfry1 <tyler.fry1@outlook.com>
2023-12-03 14:40:09 -0700

    Revised Commit

--- movie-watchlist/src/App.js
+++ movie-watchlist/src/App.js
@@ -1,31 +1,51 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./componets/Header";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [list, setList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const addMovie = (movie) => setList([...list, movie]);
+
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setList(newState);
+  };
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
       <Header />
+      <main>
+        <MovieScreen
+          addMovie={addMovie}
+          movieList={movieList}
+          page={page}
+          setPage={setPage}
+          list={list}
+          removeMovie={removeMovie}
+        />
+        <Watchlist list={list} removeMovie={removeMovie} />
+      </main>
     </div>
   );
 }
